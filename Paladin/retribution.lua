cb8c71f85b85110f92895bc08bb98a31b49fd93d
Lont-PN <lont@project-nexus.net>
2014-11-05 19:54:34 +0000

    First Rotation Added
    The first rotation added:
    Paladin - Retribution: Rev 0.1.0

---
+++ Paladin/retribution.lua
@@ -0,0 +1,21 @@
+-- Lont's Paladin/Retribution Rotation Version 0.1.0
+ProbablyEngine.rotation.register_custom(70, "ProjectNexus", {
+
+-- Cooldowns
+
+{"31884", "modifier.cooldowns" },
+{"105809", {"modifier.cooldowns",
+			"player.holypower < 3" } },
+--{#76095", "modifier.cooldowns" }, -- Uses Potion of Mogu Power
+
+-- Combat Rotation
+
+{"85256", "player.holypower >= 3"}, -- Templar's Verdict
+{"85256", {"player.buff(31884)", "player.buff(105809)", } },
+{"35395", },
+{"20271", },
+{"879", },
+{"114157", },
+{"24275", },
+
+})
